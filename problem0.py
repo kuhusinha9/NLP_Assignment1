7025cb70b233ba29d2aec330862b7cabe4183ded
kuhusinha9 <kuhusinha92@gmail.com>
2023-04-19 16:09:21 +0200

    finished assignment 0

--- problem0.py
+++ problem0.py
@@ -1,9 +1,8 @@
-import nltk
+import nltk, re, itertools
 import matplotlib.pyplot as plt
 import numpy as np
 from nltk.corpus import brown
 from nltk import word_tokenize
-import itertools
 
 # We chose lore and science_fiction
 genres = ['lore', 'science_fiction']
@@ -20,12 +19,15 @@ plt.plot([x[0] for x in count], [x[1] for x in count])
 
 
 for genre in genres:
-    words = brown.words(categories = genre)
     sentences = brown.sents(categories = genre)
     types = brown.tagged_words(categories = genre)
     
     # tokenize
-    tokens = words
+    tokens = brown.words(categories = genre)
+    words = []
+    for token in tokens:
+        if re.search('^\w',token):
+            words.append(token)
 
     #Number of tokens
     num_of_tokens = len(tokens)
@@ -37,9 +39,13 @@ for genre in genres:
     print(f'The number of unique types in the {genre} is: {num_of_types}')
 
     #Number of words
-    
+    print(f'The number of words in the {genre} is: {len(words)}')
 
     #Average number of words per sentence
+    sentence_lengths = []
+    for sent in sentences:
+        sentence_lengths.append(len(sent))
+    print(f'The average sentence length in the {genre} genre is: {np.mean(sentence_lengths)}')
 
     #Average word length. 
     word_lengths = []
@@ -48,6 +54,11 @@ for genre in genres:
     print(f'The average word length in the {genre} genre is: {np.mean(word_lengths)}')
     
     #Default part-of-speech tagger on the dataset and identify the ten most frequent POS tags.
-    tags = brown.tagged_words()
-
-    
+    tagged_words = brown.tagged_words()
+    tags= {}
+    for tag in tagged_words:
+        if tag[1] not in tags.keys():
+            tags[tag[1]]=0
+        tags[tag[1]]+=1
+    tags = sorted(tags.items(), key=lambda item: item[1], reverse=True)
+    for i in range(0,10): print(tags[i])
