ee7f9e2169948d2c6fc1f19876e908679ed4303e
kuhusinha9 <kuhusinha92@gmail.com>
2023-04-19 21:48:06 +0200

    bigram skelaton

--- problem3.py
+++ problem3.py
@@ -18,16 +18,28 @@ from problem1 import word_index_dict
 vocab = open("brown_vocab_100.txt")
 f = open("brown_100.txt")
 
-#TODO: initialize numpy 0s array
-counts = np.matrix(word_index_dict)
-print(counts)
+#initialize numpy 0s array
+counts = np.zeros((len(word_index_dict),len(word_index_dict)))
+
+# tokenise brown_100 words
+words=[]
+for line in f:
+    text = line.split()
+    for i in text:
+        words.append(i.lower())
 
 #TODO: iterate through file and update counts
+prev_word = '<s>'
+for word in words:
+    counts[0][0] += 1
 
-#TODO: normalize counts
 
-#TODO: writeout bigram probabilities to bigram_probs.txt
+#normalize counts
+probs = normalize(counts, norm='l1', axis=1)
+
+#writeout bigram probabilities to bigram_probs.txt
 bigrams = open("bigram_probs.txt", 'w')
+bigrams.write(str(probs))
 
 
 bigrams.close()
