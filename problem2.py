7cea1be1a6575e53d28dd3812ea59f176afa4700
MerelFlorian <merelflorian146@gmail.com>
2023-04-20 11:07:43 +0200

    problem2 finished

--- problem2.py
+++ problem2.py
@@ -7,26 +7,31 @@ NLP A2: N-Gram Language Models
 
 DO NOT SHARE/DISTRIBUTE SOLUTIONS WITHOUT THE INSTRUCTOR'S PERMISSION
 """
-
 import numpy as np
 from generate import GENERATE
 
-
 vocab = open("brown_vocab_100.txt")
 
-#load the indices dictionary
+# load the indices dictionary
 word_index_dict={line.rstrip(): i for i, line in enumerate(vocab)}
 
+# open file
 f = open("brown_100.txt")
 
-counts = []#TODO: initialize counts to a zero vector
-counts=np.zeros()
+# initialize counts to a zero vector
+counts=np.zeros(len(word_index_dict))
 
-#TODO: iterate through file and update counts
+# iterate through file and update counts
+for sentence in f:
+    words=[word.lower()for word in sentence.split() if word != "<s>" and word != "</s>"] 
+    for word in sentence.split():
+        counts[word_index_dict[word.lower()]]+=1
 
+proportion= np.count_nonzero(counts == 1)/len(word_index_dict)
+
+# close file
 f.close()
 
-#TODO: normalize and writeout counts. 
-
-
-
+# normalize and writeout counts. 
+probs = counts / np.sum(counts)
+np.savetxt("unigram_probs.txt", probs)
